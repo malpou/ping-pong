37079ec150aac8b4cddf872f075a89f225c76d33
Malthe Poulsen <malthe@grundtvigsvej.dk>
2024-12-26 17:42:32 +0100

    make sure paddles are moved in the colision logic

--- server/domain/paddle.py
+++ server/domain/paddle.py
@@ -5,23 +5,26 @@ from domain.ball import Ball
 
 @dataclass
 class Paddle:
-    x_position: float
+    x_position: float # Position as percentage of screen height (0-1)
     y_position: float = 0.5  # Position as percentage of screen height (0-1)
     height: float = 0.2  # Height as percentage of screen height
     width: float = 0.02 # Width of the paddle
     speed: float = 0.02  # Movement speed per frame
 
-
     def __init__(self, x_pos: float):
         self.x_position = x_pos
         self.h = self.height / 2
-        self.y_min = self.y_position - self.h
-        self.y_max = self.y_position + self.h
+
+    @property
+    def y_min(self) -> float:
+        return self.y_position - self.h
+
+    @property
+    def y_max(self) -> float:
+        return self.y_position + self.h
 
     def is_on_paddle(self, ball: Ball) -> bool:
-        # Check if the ball is near the paddle in the horizontal direction (x-axis)
-        if np.abs(ball.x - self.x_position) <=  ball.radius + self.width / 2:
-            # Check if the ball is within the vertical range of the paddle
+        if np.abs(ball.x - self.x_position) <= ball.radius + self.width / 2:
             if self.y_min - ball.radius <= ball.y <= self.y_max + ball.radius:
                 return True
         return False
@@ -31,5 +34,5 @@ class Paddle:
         self.y_position = max(self.h, new_y)
 
     def move_down(self) -> None:
-        new_y = self.y_position + self.speed
+        new_y = self.y_position + self.speed 
         self.y_position = min(1.0 - self.h, new_y)
